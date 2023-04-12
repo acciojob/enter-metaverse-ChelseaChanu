e7b135ddf226f5cae4cb814a1402d91a15b1390e
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-12 20:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const ptag = document.getElementById("status");
+const upgrad = document.getElementById("update");
+const btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click",()=>{
+	ptag.style.display = "none";
+	upgrad.style.display = "block";
+	upgrad.textContent = "Entered Metaverse";
+});
